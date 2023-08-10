169806c78b25b3879bbea4a96324d54682627591
fwesterath <fwesterath@ethz.ch>
2023-08-10 16:20:24 +0200

    Update config files

--- src/main/java/ch/fwesterath/logisticsapi/LogisticsAPIApplication.java
+++ src/main/java/ch/fwesterath/logisticsapi/LogisticsAPIApplication.java
@@ -2,10 +2,24 @@ package ch.fwesterath.logisticsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LogisticsAPIApplication {
     public static void main(String[] args) {
         SpringApplication.run(LogisticsAPIApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            @CrossOrigin(origins = "*")
+            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*");
+            }
+        };
     }
 }
